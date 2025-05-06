b9e3d2f9de95d41d54fb2f51fb75d3663945d86e
Erickson <rixon.code@gmail.com>
2025-05-07 01:58:05 +0800

    Fixed the error in Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,25 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import './globals.css';
+import { StatusBar } from 'react-native';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+    <>
+      <StatusBar hidden={true} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(onboarding)/index" />
+        <Stack.Screen name="(onboarding)/onboarding" />
+        <Stack.Screen name="(onboarding)/roleSelect" />
+        <Stack.Screen name="screens/actionSelect" />
+        <Stack.Screen name="screens/checkRouteList" />
+        <Stack.Screen name="screens/waitRouteList" />
+        <Stack.Screen name="screens/routes/checkRoute[Id]" />
+        <Stack.Screen name="screens/selectStop" />
+        <Stack.Screen name="screens/routes/waitRoute[Id]" />
+        <Stack.Screen name="screens/conductorScreens/register" />
+        <Stack.Screen name="screens/conductorScreens/login" />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }

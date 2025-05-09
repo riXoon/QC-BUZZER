7e201e9342cbe209520d2c09a3b6b512650b2364
Erickson <rixon.code@gmail.com>
2025-05-10 03:16:47 +0800

    added the notification  handler in order for the push notif to work in this environment

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,45 @@
 import { Stack } from 'expo-router';
 import './globals.css';
 import { StatusBar } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true, 
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function RootLayout() {
+  useEffect(() => {
+    // Request notification permissions
+    const askForNotificationPermissions = async () => {
+      const { status } = await Notifications.requestPermissionsAsync();
+      if (status === 'granted') {
+        console.log('Notification permissions granted!');
+      } else {
+        console.log('Notification permissions denied');
+      }
+    };
+    askForNotificationPermissions();
+
+    const notificationReceivedListener = Notifications.addNotificationReceivedListener(notification => {
+      console.log('Notification received while app is in foreground:', notification);
+    });
+
+    const notificationResponseListener = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log('User clicked on the notification:', response);
+    });
+
+  
+    return () => {
+      notificationReceivedListener.remove();
+      notificationResponseListener.remove();
+    };
+  }, []);
+
   return (
     <>
       <StatusBar hidden={true} />
